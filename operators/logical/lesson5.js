d0cf77acfd36fcdab1e9792161710f457252204f
meduza227 <meduza227@gmail.com>
2025-10-02 15:56:02 +0300

    operators

--- operators/logical/lesson5.js
+++ operators/logical/lesson5.js
@@ -1,2 +1,21 @@
 // Logical "AND"
 console.log(true && true) // all values have to be TRUE for expration to be TRUE
+
+// Logical "OR"
+console.log(true || false) // any value should be TRUE for expration to be TRUE
+
+var ageIsMoreThanEighteen = true
+var isUSCitizen = false
+
+var eligibilityForDriverLicense = ageIsMoreThanEighteen && isUSCitizen
+console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
+
+
+var eligibilityForDriverLicense = ageIsMoreThanEighteen || isUSCitizen
+console.log('This customer is eligible for DL: ' + eligibilityForDriverLicense)
+
+//Logical "NOT"
+
+console.log(!true)
+
+console.log(6 !== 10)
