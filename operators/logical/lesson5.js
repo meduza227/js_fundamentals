66653237a70b1ea9f370817ea45dbd856091f833
meduza227 <meduza227@gmail.com>
2025-10-02 15:43:52 +0300

    concatenation and interpolation

---
+++ operators/logical/lesson5.js
@@ -0,0 +1,2 @@
+// Logical "AND"
+console.log(true && true) // all values have to be TRUE for expration to be TRUE
---
+++ operators/relational_and_equality/lesson4.js
@@ -0,0 +1,16 @@
+//Relational or comparison operator
+
+var result = 10 > 5
+console.log(result)
+
+var result = 10 > 15
+console.log(result)
+
+var result = 5 >= 5
+console.log(result)
+
+//equality operators
+console.log("---------------")
+var x = 1
+console.log(x == '1') //lose comparasion -> compare just value
+console.log(x === '1') // strict comparasion -> not just value , compare also data types -> should be equil by the value and data type
