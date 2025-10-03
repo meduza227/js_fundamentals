9a2ef4ccd97f4594d9f95d6a5183c062a9c9f37a
meduza227 <meduza227@gmail.com>
2025-10-03 10:12:44 +0300

    class and methods

---
+++ class_and_methods/lesson9.js
@@ -0,0 +1,6 @@
+import { customerDetails } from "../helpers/printHelper.js";
+
+// var customerDetails = new CustomerDetails()
+
+customerDetails.printFirstName('Steve')
+customerDetails.printLastName('Law')
--- helpers/printHelper.js
+++ helpers/printHelper.js
@@ -1,3 +1,21 @@
 export function printAge(age){
     console.log(age)
 }
+
+export class CustomerDetails{
+
+    printFirstName(firstName){
+        console.log(firstName)
+    }
+
+    /**
+     * This method will print the last name
+     * @param {string} lastName 
+     */
+    printLastName(lastName){
+        console.log(lastName)
+    }
+
+}
+
+export const customerDetails = new CustomerDetails()
