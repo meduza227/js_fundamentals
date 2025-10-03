07163a3591fd5e34bf2dacf89db5deb506c4ecfb
meduza227 <meduza227@gmail.com>
2025-10-03 11:05:36 +0300

    refactor

---
+++ js_fundamentals/typeScript_vs_javaScript/lesson10.ts
@@ -0,0 +1,14 @@
+
+var customerFirstName: string = "John"
+var customerLastName: string = "Smith"
+var customerAge: number = 25
+
+type Customer = {firstName: string, lastName: string, active: boolean}
+
+var firstCustomer: Customer = {
+    firstName: "mary",
+    lastName:"smith",
+    active: true
+}
+
+console.log(firstCustomer)
